a1f656b1f005b8d5dc846cad69ae8060ab4e8687
unknown <2021cs207@stu.ucsc.cmb.ac.lk>
2023-07-30 16:26:19 +0530

    Initial Commit

---
+++ bookfind/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./Components/NavBar";
+
+
+function App() {
+  return (
+    <React.Fragment>
+      <Navbar />
+    </React.Fragment>
+    
+  );
+}
+
+export default App;
