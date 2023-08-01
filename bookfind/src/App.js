b67007223978532b8e6dd9ffe2e0374929e1cdb4
unknown <2021cs207@stu.ucsc.cmb.ac.lk>
2023-08-02 00:48:47 +0530

    Changes to hero page

--- bookfind/src/App.js
+++ bookfind/src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Navbar from "./Components/NavBar";
-
+import Body from "./Components/BodyContent";
+import Bookstore from "./Components/BookStore";
 
 function App() {
   return (
     <React.Fragment>
       <Navbar />
+      <Body />
+      <Bookstore />
     </React.Fragment>
     
   );
---
+++ bookfind/src/Components/BodyContent.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/Styles.css";
+
+function Body(){
+    return(
+        <body>
+            <div className="content">
+                <p className="para1">e-Books | Explore | Discover</p>
+                <h1 className="heading1">IT'S NOT JUST A<br></br>BOOKSTORE.</h1>
+                <p className="para2">It's a universe.</p>
+            </div>
+        </body>
+    );
+}
+
+export default Body
