6657e64b7d3d78b9ab8cb9c354f98d0b19b05799
unknown <2021cs207@stu.ucsc.cmb.ac.lk>
2023-08-30 11:53:43 +0530

    Book.js - Individual Book Details Page

--- bookfind/src/App.js
+++ bookfind/src/App.js
@@ -3,7 +3,6 @@ import Navbar from "./Components/NavBar";
 import Body from "./Components/BodyContent";
 import TrendingBooks from "./Components/TrendingBooks";
 import BookRec from "./Components/BookRec";
-// import Bookstore from "./Components/BookStore";
 import './styles/Styles.css'
 
 function App() {
@@ -13,7 +12,6 @@ function App() {
       <Body />
       <TrendingBooks />
       <BookRec />
-      {/* <Bookstore /> */}
     </React.Fragment>
     
   );
